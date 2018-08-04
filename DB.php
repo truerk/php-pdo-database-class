e06df6cd8a2b96f6ab09699b491b2ded28b3032d
truerk <truerk@yandex.ru>
2018-08-04 20:06:16 +0500

    dbQuery. clearParameters. Destruct.

--- DB.php
+++ DB.php
@@ -7,23 +7,23 @@
 
 class DB
 {
-
-    private $connection;
+    private $pdo;#Переменная для подключения к базе данных
     private $host = "localhost";
     private $dbname = "bd_test";
     private $charset = "utf8";
     private $uname = "root";
     private $upass = "";
-    private $connect_check = false;
-    private $query;
-    private $parametrs = [];
+    private $connectCheck = false;#Переменная для проверки подключения базы данных
+    private $query;#Переменная для работы с запросом
+    private $rowOperator;#Запрос в виде массива
+    private $operator;#Оператор sql
     private $dsn;
     private $opt = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_EMULATE_PREPARES => false
     ];
 
-
+    #=================================================================================================================#
     /**
      * DB constructor.
      */
@@ -32,63 +32,136 @@ class DB
         $this->dbconnect();
     }
 
+    #=================================================================================================================#
     /**
-     * DB connect
+     * DB destruct.
+     */
+    function __destruct()
+    {
+        $this->dbClose();
+    }
+
+    #=================================================================================================================#
+    /**
+     * DB connect - подключение к базе данных
      */
     function dbConnect()
     {
         $this->dsn = "mysql:host=$this->host; dbname=$this->dbname; charset=$this->charset";
         try {
-            $this->connection = new PDO($this->dsn, $this->uname, $this->upass, $this->opt);
-            $this->connect_check = true;
+            $this->pdo = new PDO($this->dsn, $this->uname, $this->upass, $this->opt);
+            $this->connectCheck = true;
         } catch (Exception $e) {
-            throw new Exception('Ошибка подключения базы даныых!');;
+            throw new Exception("Ошибка подключения базы даныых!");;
             die();
         }
     }
 
+    #=================================================================================================================#
     /**
-     * DB query
-     * @param $sql
-     * @param null $param
+     * Метод отключения базы данных
+     */
+    function dbClose(){
+        $this->pdo = null;
+    }
+
+    #=================================================================================================================#
+    /**
+     * Метод запроса к базе данных
+     * @param $sql - sql запрос
+     * @param null $param - pdo параметры
+     * @param string $mode
      * @return mixed
      * @throws Exception
      */
-    function dbQuery($sql, $param = null)
+    function dbQuery($sql, $param = null, $mode = "fetchAll")
     {
-        //если бд не подключена, подключаемся
-        if (!$this->connect_check) {
+        #Если бд не подключена, подключаемся
+        if (!$this->connectCheck) {
             $this->dbconnect();
         }
 
-        //Подготавливаем запрос
+        #Проверка mode
+        switch ($mode) {
+            case "fetchAll":
+                $mode = "fetchAll";
+                break;
+            case "fetchall":
+                $mode = "fetchAll";
+                break;
+            case "fetch":
+                $mode = "fetch";
+                break;
+            default:
+                throw new Exception("Неправильно задан мод!");
+                die();
+        }
+
+        #Проверяем какой пришел запрос
+        $this->rowOperator = explode(" ", $sql);
+        $this->operator = strtolower($this->rowOperator[0]);
+
+        #Подготавливаем запрос
         try {
-            $this->query = $this->connection->prepare($sql);
+            $this->query = $this->pdo->prepare($sql);
         } catch (Exception $e) {
-            throw new Exception('Ошибка в запросе!');
+            throw new Exception("Ошибка в запросе!");
             die();
         }
 
-        //Если есть параметры, биндим их
+        #Если есть параметры, биндим их
         if ($param != null and is_array($param)) {
+            #Чистим от пробелов и тегов html
+            $param = $this->clearParameters($param);
             foreach ($param as $key => $value) {
                 try {
                     $this->query->bindParam($key, $value);
                 } catch (Exception $e) {
-                    throw new Exception('Ошибка в параметрах запроса!');
+                    throw new Exception("Ошибка в параметрах запроса!");
                     die();
                 }
             }
         }
 
-        //Выполняем запрос, если все хорошо отправляем данные
-
+        #Выполняем запрос, если все хорошо отправляем данные
         if (!$this->query->execute()) {
-            throw new Exception('Запрос не выполнен!');
+            throw new Exception("Запрос не выполнен!");
             die();
         } else {
-            return $this->query->fetchAll();
+            #Проверяем оператора и возвращаем данные
+            if ($this->operator === "select") {
+                return $this->query->$mode();
+            } elseif ($this->operator === "insert" || $this->operator === "update" || $this->operator === "delete") {
+                return $this->query->rowCount();
+            } else {
+                return null;
+            }
         }
     }
+
+    #=================================================================================================================#
+    /**
+     * Метод возврата последнего добавленого id
+     * @return mixed
+     */
+    function dbLastId()
+    {
+        return $this->pdo->lastInsertId();
+    }
+
+    #=================================================================================================================#
+    /**
+     * Метод очистки от пробелов и html тегов
+     * @param $param
+     * @return mixed
+     */
+    private function clearParameters($param)
+    {
+        foreach ($param as $key => $value) {
+            $param[$key] = htmlspecialchars(trim($value));
+        }
+        return $param;
+    }
+
 
 }
--- README.md
+++ README.md
@@ -5,22 +5,34 @@
 <?php 
     require_once('DB.php');
 ``` 
-
 ##### 2. Обьект класса:
 ```php
 <?php 
     $db = new DB();
-``` 
- 
+```  
 ##### 3. Использовать общий запрос:
 ```php
 <?php 
     $users = $db->dbQuery("SELECT * FROM users");
 ``` 
-
 ##### 4. Использовать общий запрос с параметром:
 ```php
 <?php 
-    $users = $db->dbQuery("SELECT * FROM users WHERE user_name=:uname", ['uname' => 'Владислав']);
+    $users = $db->dbQuery("SELECT * FROM users WHERE user_name=:uname", ['uname' => 'Владислав'], "fetch");
+``` 
+   3 аргумент dbQuery по умолчанию fetchAll
+   
+   Параметр можно заранее вбить в массив:
+   
+```php
+<?php 
+    $data["parameter"] = value;
+``` 
+   Пример:
+```php
+<?php 
+    $data["uname"] = "Владислав";
+    $data["uemail"] = "mail@google.com"
+    $users = $db->dbQuery("SELECT * FROM users WHERE user_name=:uname and user_email=:uemail", $data, "fetch");
 ``` 
    
